445b981e0398521778102d459bd0a78f5406eac3
jorgeluiz <thejljorgeluiz@gmail.com>
2026-07-11 15:48:25 -0300

    adiciona data de criação e melhorias no CRUD

--- backend/src/main/java/bloco_notas/demo/Nota.java
+++ backend/src/main/java/bloco_notas/demo/Nota.java
@@ -1,9 +1,12 @@
 package bloco_notas.demo;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 
 @Entity //"Esta classe deve virar uma tabela no banco."
 public class Nota {
@@ -11,6 +14,15 @@ public class Nota {
     @Id //É a chave primária da tabela.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @PrePersist
+    public void prePersist() {
+    dataCriacao = LocalDateTime.now();
+}
+public LocalDateTime getDataCriacao() {
+    return dataCriacao;
+}
+
+    private LocalDateTime dataCriacao;
 
     private String texto;
 
